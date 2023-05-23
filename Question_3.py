4be2195c8933eea44efc33519172ba7f1b633898
Chandrashekar <chandrashekarcp16@gmail.com>
2023-05-23 06:44:47 +0530

    Question_3

--- Question_3.py
+++ Question_3.py
@@ -1,17 +1,24 @@
-
 import requests
 import pandas as pd
+import json
 
 def download_and_convert_data(url, output_file):
     # Download the data from the provided link
     response = requests.get(url)
-    data = response.text
+    data = response.json()
 
-    # Convert the data into structured format (e.g., split by lines or separators)
-    structured_data = data.split('\n')  # Split the data by lines, adjust the separator as per the data format
+    # Extract relevant attributes from the data
+    attributes = ['id', 'num', 'name', 'img', 'type', 'height', 'weight', 'candy', 'candy_count',
+                  'egg', 'spawn_chance', 'avg_spawns', 'spawn_time', 'multipliers', 'weaknesses',
+                  'next_evolution', 'prev_evolution']
 
-    # Create a DataFrame from the structured data
-    df = pd.DataFrame(structured_data, columns=['Data'])  # Adjust column name as per the data structure
+    pokemon_data = []
+    for pokemon in data['pokemon']:
+        pokemon_info = {attr: pokemon.get(attr) for attr in attributes}
+        pokemon_data.append(pokemon_info)
+
+    # Create a DataFrame from the extracted data
+    df = pd.DataFrame(pokemon_data)
 
     # Save the DataFrame to Excel format
     df.to_excel(output_file, index=False)
@@ -19,10 +26,10 @@ def download_and_convert_data(url, output_file):
     print("Data conversion completed. Output file:", output_file)
 
 # Provide the URL of the data file to download
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data"
+url = 'https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json'
 
 # Provide the desired output file path (including the .xlsx extension)
-output_file = 'iris.xlsx'
+output_file = 'pokedex.xlsx'
 
 # Call the function to download and convert the data
 download_and_convert_data(url, output_file)
